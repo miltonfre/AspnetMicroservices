c983075a72a4b0f2f4565a7c7dbc88618cda0489
MIlton Gonzalez <miltonfre@gmail.com>
2023-06-27 13:21:10 -0400

    Registering Application and infraestructure layer on Ordering.API

--- src/services/Ordering/Ordering.API/Program.cs
+++ src/services/Ordering/Ordering.API/Program.cs
@@ -1,4 +1,8 @@
+using Ordering.Infraestructure;
+using Ordering.Application;
+
 var builder = WebApplication.CreateBuilder(args);
+var Configuration = builder.Configuration;
 
 // Add services to the container.
 
@@ -6,6 +10,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddApplicationServices();
+builder.Services.AddInfrastructureServices(Configuration);
 
 var app = builder.Build();
 
